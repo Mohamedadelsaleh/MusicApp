5ab0851e0206f05786029d6b64483ac31552c35a
Mohamed Adel Saleh <mohamed.adel.saleh77@gmail.com>
2022-12-25 13:49:41 +0200

    Music App

---
+++ src/pages/ArtistDetails.jsx
@@ -0,0 +1,46 @@
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
+import { playPause, setActiveSong } from '../redux/features/playerSlice';
+import { useGetSongDetailsQuery, useGetSongRelatedQuery } from "../redux/services/shazamCore";
+
+const ArtistDetails = () => {
+
+  const dispatch = useDispatch();
+  const { songid, id: artistId } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+  const { data, isFetching: isFetchinRelatedSongs, error } = useGetSongRelatedQuery({ songid });
+  
+  const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
+
+  if (isFetchingSongDetails && isFetchinRelatedSongs) return <Loader title="Searching song details" />;
+
+  if (error) return <Error />;
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  }
+
+  const handlePlayClick = (song, i) => {
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true));
+  }
+
+
+  return (
+      <div className="flex flex-col">
+          <DetailsHeader artistId={artistId} songData={songData} />
+          <RelatedSongs 
+              data={data}
+              artistId={artistId}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePauseClick={handlePauseClick}
+              handlePlayClick={handlePlayClick}
+              />
+      </div>
+  );
+};
+
+export default ArtistDetails;
